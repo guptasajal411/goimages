6c026fb5549362fb458501f6ba27b6fc78abd04c
Sajal <guptasajal411@gmail.com>
2024-09-19 15:27:42 +0530

    feat: protected routes w/ middleware

---
+++ goimages-next/src/middleware.js
@@ -0,0 +1,23 @@
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server";
+import * as jose from "jose";
+
+const shouldntBeAccessibleIfLoggedIn = ["/login", "/register"]
+const protectedRoutes = ["/dashboard"]
+
+export default async function middleware(request) {
+    const path = request.nextUrl.pathname;
+    const cookie = cookies().get(process.env.AUTH_COOKIE_NAME);
+    try {
+        const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+        const { payload } = await jose.jwtVerify(cookie.value, secret, {});
+        if (shouldntBeAccessibleIfLoggedIn.includes(path)) return NextResponse.redirect(new URL("/dashboard", request.url))
+    } catch (e) {
+        if (shouldntBeAccessibleIfLoggedIn.includes(path)) return NextResponse.next();
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+}
+
+export const config = {
+    matcher: ["/login", "/register", "/dashboard"]
+}
