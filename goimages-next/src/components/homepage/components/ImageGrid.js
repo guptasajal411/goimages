c3d66a518452e1e65449a0cefcf5ce6623c9f657
Sajal <guptasajal411@gmail.com>
2024-09-25 14:35:04 +0530

    feat: cache revalidation after upload

--- goimages-next/src/components/homepage/components/ImageGrid.js
+++ goimages-next/src/components/homepage/components/ImageGrid.js
@@ -17,7 +17,7 @@ export default async function ImageGrid() {
     try {
         const secret = new TextEncoder().encode(process.env.JWT_SECRET);
         const { payload } = await jose.jwtVerify(cookie.value, secret, {});
-        const photos = await Photo.find({ user: payload?._id }).select("s3ObjectKey").exec()
+        const photos = await Photo.find({ user: payload?._id }).sort("-createdAt").select("s3ObjectKey").exec()
         for (const photo of photos) {
             const signedUrl = s3.getSignedUrl("getObject", {
                 Bucket: process.env.S3_BUCKET_NAME,
--- goimages-next/src/components/homepage/components/UploadFiles.js
+++ goimages-next/src/components/homepage/components/UploadFiles.js
@@ -1,5 +1,6 @@
 "use client"
 
+import { revalidatePathAction } from "@/actions/utilActions";
 import { useState } from "react"
 import toast from "react-hot-toast";
 
@@ -19,11 +20,12 @@ export default function UploadFiles() {
         });
         const response = await _response.json();
         response?.success ? toast.success(response.message) : toast.error(response.message);
+        revalidatePathAction("/");
         return 0;
     }
 
     return <form onSubmit={async e => await handleUpload(e)}>
-        <input type="file" multiple onChange={e => handleFileChange(e)} />
+        <input type="file" multiple accept="image/*" onChange={e => handleFileChange(e)} />
         <button type="submit" className="border-2 border-black px-2">Upload</button>
     </form>
 }
