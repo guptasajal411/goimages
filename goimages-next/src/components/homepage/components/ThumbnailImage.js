b1d358e7e65a3f214c26c0f8eaa201913e944c0c
Sajal <guptasajal411@gmail.com>
2024-09-25 16:46:04 +0530

    fix: onLoadingComplete deprecation

--- goimages-next/src/components/homepage/components/ThumbnailImage.js
+++ goimages-next/src/components/homepage/components/ThumbnailImage.js
@@ -5,5 +5,5 @@ import { useState } from "react"
 
 export default function ThumbnailImage({ src, key }) {
     const [isLoaded, setIsLoaded] = useState(false);
-    return <Image src={src} key={key} className={`rounded h-[100%] w-auto object-cover transition-opacity duration-1000 ease-in-out ${isLoaded ? "opacity-100" : "opacity-0"}`} unoptimized width={200} height={200} onLoadingComplete={() => setIsLoaded(true)} />
+    return <Image src={src} key={key} className={`rounded h-[100%] w-auto object-cover transition-opacity duration-1000 ease-in-out ${isLoaded ? "opacity-100" : "opacity-0"}`} unoptimized width={200} height={200} onLoad={() => setIsLoaded(true)} />
 }
