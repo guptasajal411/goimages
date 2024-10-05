7039bbf87e0e72461b39713f37a249246ab77743
Sajal <guptasajal411@gmail.com>
2024-10-06 01:47:35 +0530

    chore: add github link

--- goimages-next/src/components/homepage/home.js
+++ goimages-next/src/components/homepage/home.js
@@ -14,10 +14,15 @@ export default async function Home() {
                 <h1 className={`${libreBaskerville.className} block text-3xl text-primary text-center sm:text-start`}>Your Self-Hosted Photo Library</h1>
                 <div className="max-w-[75%] flex flex-col">
                     <p className="text-lg text-secondary text-center sm:text-start">Free, open-source, and private photo storage, powered by your own S3 bucket</p>
-                    <Link className="min-w-full bg-lime-400 bg-opacity-10 hover:bg-opacity-15 transition-all ease-in-out duration-200 text-center text-primary mt-3 py-2 px-2 rounded-md border-lime-500/100 border flex justify-center items-center gap-2" href="/register">
-                        <>Get started</>
-                        <Image className="opacity-70" src="/open.svg" width="16" height="16" />
-                    </Link>
+                    <div className="min-w-full flex">
+                        <Link className="min-w-full bg-lime-400 bg-opacity-10 hover:bg-opacity-15 transition-all ease-in-out duration-200 text-center text-primary mt-3 py-2 px-2 rounded-md border-lime-500/100 border flex justify-center items-center gap-2" href="/register">
+                            <>Get started</>
+                            <Image className="opacity-70" src="/open.svg" width="16" height="16" />
+                        </Link>
+                        <Link className="min-w-full sm:block hidden opacity-80 hover:opacity-100 transition-all ease-in-out duration-200 mt-3 py-2 px-2 ms-2" href="https://github.com/guptasajal411/goimages" target="_blank">
+                            <Image src="/github.svg" width="32" height="32" />
+                        </Link>
+                    </div>
                 </div>
             </div>
             <div className="sm:flex hidden sm:basis-1/2 flex-row items-center justify-center">
