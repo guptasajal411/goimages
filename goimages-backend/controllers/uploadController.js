f667f073eaa9d7091a8b8d8d789a4bdc11a974b5
Sajal <guptasajal411@gmail.com>
2024-10-06 17:38:50 +0530

    fix: response cycle

--- goimages-backend/controllers/uploadController.js
+++ goimages-backend/controllers/uploadController.js
@@ -15,22 +15,22 @@ export const uploadFiles = async (req, res) => {
             // const queueResponse = await photoQueue.add(`photo:${req.user._id}:${photo._id}`, file);
             const s3Response = await bufferToS3(file.buffer, req.user._id, photo?._id, fileFormat);
             if (s3Response.success) {
+                const { height, width } = await sharp(file.buffer).metadata();
+                photo.s3ObjectKey = s3Response?.s3ObjectKey;
+                photo.height = height; photo.width = width;
                 await photo.save();
                 const metadata = parser.create(file.buffer);
                 const parsedMetadata = metadata.parse();
                 if (parsedMetadata?.tags?.["DateTimeOriginal"] || parsedMetadata?.tags?.["CreateDate"]) {
                     photo.createTime = new Date((parsedMetadata?.tags?.["DateTimeOriginal"] || parsedMetadata?.tags?.["CreateDate"]) * 1000)
                 }
-                const { height, width } = await sharp(file.buffer).metadata();
-                photo.s3ObjectKey = s3Response?.s3ObjectKey;
-                photo.height = height; photo.width = width;
                 await photo.save();
             } else {
                 throw new Error("An error occoured")
             }
         } catch (e) {
             console.log(e);
-            return res.status(500).json({ success: false, message: "An error occoured" })
+            // return res.status(500).json({ success: false, message: "An error occoured" })
         }
     }
     return res.status(200).json({ success: true, message: "Images backed up to the cloud" })
