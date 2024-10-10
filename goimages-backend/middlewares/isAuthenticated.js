58ce114dcb5376eeff5c95b5cc51070a6a170af4
Sajal <guptasajal411@gmail.com>
2024-10-10 21:07:54 +0530

    fix: auth middleware for crossorigin reqs

--- goimages-backend/middlewares/isAuthenticated.js
+++ goimages-backend/middlewares/isAuthenticated.js
@@ -3,13 +3,14 @@ import User from "../models/User.js";
 
 export default async function isAuthenticated(req, res, next) {
     const cookie = req.cookies[process.env.AUTH_COOKIE_NAME];
-    const header = req.header["authorization"]
+    const header = req.headers["authorization"] || req.headers["Authorization"];
     if (!cookie && !header) {
-        return res.status(401).json({ success: false, message: "unauthorised", devMessage: "cookie not found" })
+        return res.status(401).json({ success: false, message: "unauthorised", devMessage: "cookie/auth header not found" })
     }
     try {
         const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-        const { payload } = await jose.jwtVerify(cookie, secret, {});
+        const token = cookie ? cookie : header.split(" ")[1];
+        const { payload } = await jose.jwtVerify(token, secret, {});
         const foundUser = await User.findOne({ email: payload?.email }).exec();
         if (!foundUser) throw new Error("user not found in db with email: " + payload?.email)
         req.user = { name: foundUser.name, email: foundUser.email, _id: foundUser._id };
